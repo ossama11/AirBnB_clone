ab2cd0b0bd4c30b2e35394c2487269cbd6dd5f72
ossama11 <jeuxossama67@gmail.com>
2024-03-18 12:04:32 +0000

    test 3

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,18 @@
+-- Use the root user to perform administrative tasks
+-- Make sure to replace 'root_password' with your actual root password
+
+-- Create the 'hbnb_dev_db' database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+
+-- Create the 'hbnb_dev' user with the password 'hbnb_dev_pwd' if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on 'hbnb_dev_db' to 'hbnb_dev'
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on 'performance_schema' to 'hbnb_dev'
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Apply the changes made by the GRANT statements
+FLUSH PRIVILEGES;
